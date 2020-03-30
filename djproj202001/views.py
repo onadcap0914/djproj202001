3d107ec6ff319e0144782eafcd6fe6fac5849813
onadcap0914 <onadcap0914@gmail.com>
2020-03-30 13:35:23 +0800

    [4] onad | 20200330 | Application Home page and sign up functionality * Created Home page.

--- djproj202001/views.py
+++ djproj202001/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 """
 [2] onad | 20200324 | File first update
 [3] onad | 20200326 | improvement of [2]
+[4] onad | 20200330 | Application Home page and sign up functionality
 """
 # Create your views here.
 
 
 def home(request):
-    # title = 'CEU CCI - Loan Management System'
-    # return render(request, 'home.html', {'title': title})   #[3]
-    return render(request, 'base_bootstrap_hdr_stky-ftr.html')
+    title = 'CEU Credit Cooperative Management System'
+    return render(request, 'home.html', {'title': title})   #[3] [4]
+
