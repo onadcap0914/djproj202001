16ade72594ee9219678034eb3783388a2289f55d
onadcap0914 <onadcap0914@gmail.com>
2020-04-04 12:56:43 +0800

    [10] onad | 20200401 | Bootstrap4 forms Sign up form Password reset form Password reset confirm form Password change form

--- djproj202001/views.py
+++ djproj202001/views.py
@@ -8,6 +8,8 @@ from django.contrib.auth.models import User
 [3] onad | 20200326 | improvement of [2]
 [4] onad | 20200330 | Application Home page and sign up functionality
 [9] onad | 20200401 | Protected/secured views
+[10] onad | 20200401 | Bootstrap4 forms, added check if user is already authenticated 
+        then direct them to the right page. 
 """
 # Create your views here.
 
@@ -15,18 +17,20 @@ from django.contrib.auth.models import User
 def home(request):
     if request.user.is_authenticated:
         return redirect('logged_in')
-    else:
-        title = 'CEU Credit Cooperative Management System'
-        return render(request, 'home.html', {'title': title})   #[3] [4]
+
+    title = 'CEU Credit Cooperative Management System'
+    return render(request, 'home.html', {'title': title})   #[3] [4]
 
 
 def signup(request):    #[4]
+    if request.user.is_authenticated:
+        return redirect('logged_in')
     title = 'CEU CC Management System User Registration'
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('site_home')
+            return redirect('logged_in')
     else:
         form = UserCreationForm()
 
